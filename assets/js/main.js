1a79f76e27686e7b62ff92e42a6ffa6821aab37f
Habib Rahman <45137768+Dark-Knight-HR@users.noreply.github.com>
2019-02-28 07:26:05 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,30 +3,54 @@
 
 	jQuery(document).ready(function ($) {
 		new WOW().init();
+		// Magnific Popup starts
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			gallery: {
+					enabled: true
+				}
+		})
 
 		// Owl carousel
 		$('.special_menu').owlCarousel({
 			items: 3,
 			loop:true,
 			autoplay:true,
-			dots: true
+			dots: true,
+			responsive: {
+				992:{
+					items: 2
+				},
+				768:{
+					items: 2
+				},
+				320:{
+					items: 1
+				}
+			}
 		});
+
 		$('.customer_carousel').owlCarousel({
 			items: 2,
 			loop:true,
 			autoplay:true,
-			dots: true
+			dots: true,
+			responsive: {
+				992:{
+					items: 2
+				},
+				768:{
+					items: 2
+				},
+				320:{
+					items: 1
+				}
+			}
 		});
 		// Owl Carousel ends
 
 		// SlickNav
 		$('#menu').slicknav();
-
-		// Type it
-		new TypeIt('.type-it', {
-		    strings: ["This will be typed!", "And this will be typed too."],
-		    loop: true
-		});
 
 	});
 
@@ -42,4 +66,17 @@
 			}
 		});
 	});
+
+	// Typed js
+
+	var typed = new Typed('#typed', {
+    strings: ["serve best food","are the best"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 20,
+    startDelay: 500,
+    cursorChar: '_',
+    smartBackspace: true
+  });
+// Typed js ends
 }(jQuery));
